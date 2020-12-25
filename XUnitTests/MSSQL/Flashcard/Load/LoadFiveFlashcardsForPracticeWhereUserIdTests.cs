31ef46bdf295125999e7665649d65edeb543a3a1
Jakub Koza <jakub.koza13@gmail.com>
2020-12-25 20:30:28 +0100

    CreateLoadFiveFcForPracticeWhereUserId
    Create implementation for retreiving flashcards for practice depends on userId not on group so from every flashcard user created.

---
+++ XUnitTests/MSSQL/Flashcard/Load/LoadFiveFlashcardsForPracticeWhereUserIdTests.cs
@@ -0,0 +1,38 @@
+﻿using DatabaseModule;
+using Models;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace XUnitTests.MSSQL
+{
+    [Collection("Sequential")]
+    public class LoadFiveFlashcardsForPracticeWhereUserIdTests
+    {
+        [Fact]
+        public void LoadFiveFlashcardsForPracticeWhereUserIdTestA()
+        {
+            ResetTestDatabasePostgreSQLv4.Reset();
+
+            LoadFiveFlashcardsForPracticeWhereUserId _processor = new LoadFiveFlashcardsForPracticeWhereUserId();
+
+            var output = _processor.Load("c4a82913-7936-4448-a9e9-d33e5796a414");
+
+            Assert.NotNull(output.
+                FirstOrDefault(e => 
+                e.Id == Guid.Parse("45433894-5820-413F-93FB-46429BA8486A") && e.PracticeDirection == PracticeDirection.FromForeignToNative));
+
+            Assert.NotNull(output.
+                FirstOrDefault(e => 
+                e.Id == Guid.Parse("0B6799B7-9734-49C2-B701-770AD9601284") && e.PracticeDirection == PracticeDirection.FromNativeToForeign));
+
+            Assert.NotNull(output.
+                FirstOrDefault(e =>
+                e.Id == Guid.Parse("58D47AAF-DDCD-47D0-B8A1-4FEBD4E24C7F") && e.PracticeDirection == PracticeDirection.FromNativeToForeign));
+
+            Assert.Null(output.
+                FirstOrDefault(e =>
+                e.Id == Guid.Parse("DEADEC4A-1FA4-4ED8-809D-B92EB5FECC6C") && e.PracticeDirection == PracticeDirection.FromNativeToForeign));
+        }
+    }
+}
