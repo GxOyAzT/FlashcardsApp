7be99d7f6eb0c59915905eace173bbd07f6c4d3d
Jakub Koza <jakub.koza13@gmail.com>
2021-02-06 00:30:39 +0100

    Fix bugs

---
+++ DatabaseModuleInterface/FlashcradModels/Count/ICountFlashcardsForPractice.cs
@@ -0,0 +1,7 @@
+﻿namespace DatabaseModuleInterface
+{
+    public interface ICountFlashcardsForPractice
+    {
+        public int Count(string userId);
+    }
+}
---
+++ XUnitTests/MSSQL/Flashcard/Count/CountFlashcardsForPracticeTests.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseModule;
+using Xunit;
+
+namespace XUnitTests.MSSQL
+{
+    [Collection("Sequential")]
+    public class CountFlashcardsForPracticeTests
+    {
+        [Fact]
+        public void CountFlashcardsForPracticeTestA()
+        {
+            ResetTestDatabasev6.Reset();
+
+            var _process = new CountFlashcardsForPractice();
+
+            Assert.True(_process.Count("00782523-7206-403a-b953-75cfa7ccb4e1") == 6);
+        }
+    }
+}
